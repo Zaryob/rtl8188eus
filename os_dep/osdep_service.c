cf0fd404a7a5bf2a3f3ca0cf5403ff522b53ea10
Suleyman Poyraz <zaryob.dev@gmail.com>
2021-02-12 10:35:31 +0300

    Fixed __vfs_read error
    Signed-off-by: Suleyman Poyraz <zaryob.dev@gmail.com>

--- os_dep/osdep_service.c
+++ os_dep/osdep_service.c
@@ -1987,8 +1987,10 @@ static int readFile(struct file *fp, char *buf, int len)
 		return -EPERM;
 
 	while (sum < len) {
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 1, 0))
-		rlen = __vfs_read(fp, buf + sum, len - sum, &fp->f_pos);
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 14, 0))
+		rlen = kernel_read(fp, buf + sum, len - sum, &fp->f_pos);
+#else if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 1, 0)) 
+		rlen = vfs_read(fp, buf + sum, len - sum, &fp->f_pos);
 #else
 		rlen = fp->f_op->read(fp, buf + sum, len - sum, &fp->f_pos);
 #endif
