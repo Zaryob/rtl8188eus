98d73b41b4c146aeb9f425437e2cb08ba8eecbf4
Suleyman Poyraz <zaryob.dev@gmail.com>
2021-02-13 10:45:31 +0300

    Fixed make command fails with kernel 5.0.x as error monotonic boottime identifier

--- os_dep/linux/ioctl_cfg80211.c
+++ os_dep/linux/ioctl_cfg80211.c
@@ -349,9 +349,16 @@ static const struct ieee80211_txrx_stypes
 
 static u64 rtw_get_systime_us(void)
 {
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 39))
+#if (LINUX_VERSION_CODE > KERNEL_VERSION(5, 0, 0))
+        return ktime_to_us(ktime_get_boottime());
+
+#elif (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 39))
 	struct timespec ts;
+	#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 18, 0))
+	getboottime(&ts);
+	#else
 	get_monotonic_boottime(&ts);
+	#endif
 	return ((u64)ts.tv_sec * 1000000) + ts.tv_nsec / 1000;
 #else
 	struct timeval tv;
